e793b3d861d4a0ca774d79510bfa82680d172ab7
Martin Junghanns <martin.junghanns@gmx.net>
2012-10-13 19:25:02 +0200

    using csv.DictWriter instead of manually string formatting

--- src/program.py
+++ src/program.py
@@ -1,3 +1,5 @@
+import csv
+
 from SPARQLWrapper import SPARQLWrapper
 from SPARQLWrapper.SPARQLExceptions import QueryBadFormed
 
@@ -7,28 +9,21 @@ from utils import Config, Portal, SPARQLEndpoints, LMDBMovieConcept
 
 # connect to LMDB
 sparql_lmdb = SPARQLWrapper(SPARQLEndpoints.LMDB)
-
 lmdb = LMDBWrapper(sparql_lmdb)
 
-# actorCount = lmdb.get_page_count(Portal.FREEBASE, LMDBMovieConcept.ACTOR)
-
-
 def get_and_persist_lmdb_actors(f):
-    
     try:
         actorCount = lmdb.get_page_count(Portal.FREEBASE, LMDBMovieConcept.ACTOR)
         print "loading %i actors from lmdb" % actorCount
                 
         with open(f, 'w') as fOut:
+            csvwriter = csv.DictWriter(fOut, ['actorid', 'name', 'freebase'], delimiter=';') 
             for i in range(0, actorCount, Config.PAGE_SIZE):
                 print 'getting actors from %i to %i' % (i, (i + Config.PAGE_SIZE))
                 actors = lmdb.get_actors(Portal.FREEBASE, 
                                             i, # offset
                                             Config.PAGE_SIZE) # limit 
-                for actor in actors:                    
-                    fOut.write("%s;%s;%s\n" % (actor['actorid'], 
-                                               actor['name'], 
-                                               actor['freebase']))
+                csvwriter.writerows(actors)
                 fOut.flush()
     except IOError as ioError:
         print str(ioError)
@@ -36,23 +31,18 @@ def get_and_persist_lmdb_actors(f):
         print str(queryEx)
     
 def get_and_persist_lmdb_films(f):
-    print "loading films from lmdb"
     try:
         filmCount = lmdb.get_page_count(Portal.FREEBASE, LMDBMovieConcept.FILM)
         print "loading %i films from lmdb" % filmCount
                 
         with open(f, 'w') as fOut:
+            csvwriter = csv.DictWriter(fOut, ['filmid', 'name', 'date', 'freebase'], delimiter=';')
             for i in range(0, filmCount, Config.PAGE_SIZE):
                 print 'getting films from %i to %i' % (i, (i + Config.PAGE_SIZE))
                 films = lmdb.get_films(Portal.FREEBASE, 
                                        i, #offset 
                                        Config.PAGE_SIZE)
-                
-                for film in films:
-                    fOut.write("%s;%s;%s;%s\n" % (film['filmid'],
-                                                     film['name'],                                                     
-                                                     film['date'],
-                                                     film['freebase']))
+                csvwriter.writerows(films)
                 fOut.flush()
     except IOError as ioError:
         print str(ioError)
